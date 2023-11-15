bb24ec795419b7980bcdb648ddebdb1c65734192
thechildman <drewmarcondes@gmail.comm>
2023-11-15 12:26:00 -0600

    Adding GetMangaVolumeInfo

--- MangaReads/Controllers/MangaController.cs
+++ MangaReads/Controllers/MangaController.cs
@@ -14,10 +14,17 @@ public class MangaController : ControllerBase
         _logger = logger;
     }
 
-    private string _mangaDexUrl = "https://api.consumet.org/manga/mangadex/";
-    private string _mangaDexInfoUrl = "https://api.consumet.org/manga/mangadex/info/";
+    private const string MangaDexUrl = "https://api.consumet.org/manga/mangadex/";
+    private const string MangaDexInfoUrl = "https://api.consumet.org/manga/mangadex/info/";
 
-    HttpClient client = new();
+    HttpClient _client = new();
+
+    public class MangaVolume
+    {
+        private int id;
+        private int volumeId;
+        private string fileName;
+    }
     
     
     [HttpGet(Name = "GetMangaSearch")]
@@ -25,11 +32,45 @@ public class MangaController : ControllerBase
     {
         mangaName = mangaName.ToLower();
         
-        client.DefaultRequestHeaders.Accept.Clear();
-        client.DefaultRequestHeaders.Accept.Add(
+        _client.DefaultRequestHeaders.Accept.Clear();
+        _client.DefaultRequestHeaders.Accept.Add(
             new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
-        client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+        _client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
         
-        return await client.GetStringAsync(_mangaDexUrl + mangaName);
+        return await _client.GetStringAsync(MangaDexUrl + mangaName);
+    }
+
+    public async Task<MangaVolume[]> GetMangaVolumeInfo(int mangaId)
+    {
+        try
+        {
+            const string url1 = "https://api.mangadex.org/cover?manga%5B%5D=";
+            const string url2 = "&order%5BcreatedAt%5D=asc&order%5BupdatedAt%5D=asc&order%5Bvolume%5D=asc&limit=100";
+
+            var getVolumesInfo = await _client.GetStringAsync(url1 + mangaId + url2);
+
+            var volumeData = new MangaVolume[getVolumesInfo.Length];
+            
+            // getVolumesInfo.data.forEach(volume =>
+            // {
+            //     var volumeConverted =
+            //     {
+            //         id: volume.id,
+            //         volumeNumber: volume.attributes.volume,
+            //         fileName: volume.attributes.fileName,
+            //     }
+            //     volumeData.push(volumeConverted)
+            // });
+
+            return volumeData;
+        }
+        catch (Exception error)
+        {
+            // console.log("error")
+            // console.log(error)
+            // res.json({error, message: `Unable to fetch data on mangaVolumeInfo`})
+            //
+            return Array.Empty<MangaVolume>();
+        }
     }
 }
