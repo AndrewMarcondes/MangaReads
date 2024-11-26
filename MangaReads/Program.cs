1ac7dea38d36c8730042f27b7aad9562c5f47dcd
Andrew Marcondes <drewmarcondes@gmail.com>
2024-11-26 10:34:32 -0500

    Adding CORS

--- MangaReads/Program.cs
+++ MangaReads/Program.cs
@@ -1,9 +1,22 @@
 using MangaReads.Services;
 using MangaReads.Interfaces;
 
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddCors(options =>
+{ 
+	options.AddPolicy("AllowAll", 
+			builder => 
+			{
+				builder.AllowAnyOrigin()
+				.AllowAnyMethod()
+				.AllowAnyHeader();
+			});
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -23,6 +36,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("AllowAll");
 
 app.UseAuthorization();
 
